0d9a05268ad50203674c877cf5f583f12433b06b
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2021-01-23 12:06:08 -0500

    React init

---
+++ src/sections/landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landing.css';
+
+const LandingPage = () => {
+  return (
+      <div className="landing">
+          
+    </div>
+  );
+};
+
+export default LandingPage;		
